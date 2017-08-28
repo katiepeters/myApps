0b975e710defd35ac5d88d0b19e1a65eeb466df7
Katie Peters <katiepeters@users.noreply.github.com>
2017-08-28 15:25:34 -0400

    Update testJs.js

--- testJs.js
+++ testJs.js
@@ -1,3 +1,11 @@
 window.app = () => {
+    const react = require('react');
     console.log('Got this awesome javascript!!');
+    return () => {
+        return (
+            <div>
+                WOW
+            </div>
+        );
+    };
 };
