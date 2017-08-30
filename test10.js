6dc0f5d36f20420d53665f6bb4101f197f7a13b5
Katie Peters <katiepeters@users.noreply.github.com>
2017-08-30 10:03:28 -0400

    Create test10.js

---
+++ test10.js
@@ -0,0 +1,23 @@
+webpackJsonp([1],{
+
+/***/ "./index.js":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react__ = __webpack_require__("../node_modules/react/react.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react__);
+
+
+window.app = 'thing';
+
+const thing = 'thing';
+/* harmony export (immutable) */ __webpack_exports__["thing"] = thing;
+
+
+// export default App;
+
+/***/ })
+
+},["./index.js"]);
+//# sourceMappingURL=main.js.map
