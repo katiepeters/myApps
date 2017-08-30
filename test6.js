425c40ba3d66edd55c6bb0d03a665aaa82cd13ff
Katie Peters <katiepeters@users.noreply.github.com>
2017-08-30 09:19:01 -0400

    Create test6.js

---
+++ test6.js
@@ -0,0 +1,24 @@
+webpackJsonp([1],{
+
+/***/ "./index.js":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react__ = __webpack_require__("../node_modules/react/react.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react__);
+
+
+window.app = 'thing';
+
+module.exports = {
+  app: 'thing'
+};
+
+// export default App;
+/* WEBPACK VAR INJECTION */}.call(__webpack_exports__, __webpack_require__("../node_modules/webpack/buildin/harmony-module.js")(module)))
+
+/***/ })
+
+},["./index.js"]);
+//# sourceMappingURL=main.js.map
